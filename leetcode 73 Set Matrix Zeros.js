e7be662f180b9a1cdf2f0d7be5133b1f44fae3d3
elohim31520 <versus7063@gmail.com>
2019-09-18 18:39:18 +0800

    leetcode 73. 在排序數組中查找元素的第一個和最後一個位置

---
+++ leetcode 73 Set Matrix Zeros.js
@@ -0,0 +1,91 @@
+/**
+     * 给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
+
+    示例 1:
+
+    输入: 
+    [
+      [1,1,1],
+      [1,0,1],
+      [1,1,1]
+    ]
+    输出: 
+    [
+      [1,0,1],
+      [0,0,0],
+      [1,0,1]
+    ]
+    示例 2:
+
+    输入: 
+    [
+      [0,1,2,0],
+      [3,4,5,2],
+      [1,3,1,5]
+    ]
+    输出: 
+    [
+      [0,0,0,0],
+      [0,4,5,0],
+      [0,3,1,0]
+    ]
+    进阶:
+
+    一个直接的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+    一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+    你能想出一个常数空间的解决方案吗？
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/set-matrix-zeroes
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ */
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+// 解題: 記錄[i][0] 、[0][j] 所有的0，在把 matrix[i][j] 變為0
+var setZeroes = function(matrix) {
+    let isfirstColumnToBeZero = false
+    for (let i = 0; i < matrix.length; i++) {
+        // 第一個column被略過了
+        if(matrix[i][0] == 0) isfirstColumnToBeZero = true
+        for (let j = 1; j < matrix[0].length; j++) {
+            if(matrix[i][j] == 0) {
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+            }
+        }
+    }
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[0].length; j++) {
+            if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0
+        }
+        
+    }
+    if(matrix[0][0] == 0){
+        for (let j = 0; j < matrix[0].length; j++) {
+            matrix[0][j] = 0
+        }
+    }
+
+    if(isfirstColumnToBeZero){
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0
+        }
+    }
+    return matrix
+};
+
+console.log(setZeroes([
+    [0,1]
+]))
+
+/**
+ * expect
+ * [
+    [1,0,1],
+    [0,0,0],
+    [1,0,1]
+    ]
+ */
