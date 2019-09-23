6bcd555d05cff9b26cdd1863a53f1e7809253d5b
elohim31520 <versus7063@gmail.com>
2019-09-23 14:47:10 +0800

    35 . 搜索插入位置

--- 35. Search Insert Position.js
+++ 35. Search Insert Position.js
@@ -21,19 +21,23 @@
 
 
 var searchInsert = function(nums, target) {
-    if(nums.includes(target)) return nums.indexOf(target)
-    else {
-        let index
-        for (let i = 0; i < nums.length; i++) {
-            if(target > nums[i]){
-                index = i
-            }
-            else{
-                return i
-            }
+    let end = nums.length - 1
+    let start = 0
+    while(start <= end){
+        let mid = Math.floor((start + end) / 2)
+        if(target > nums[mid]){
+            start = mid + 1
         }
-        return index + 1
+        else if(target == nums[mid]) return mid
+        else{
+            end = mid - 1
+        }
     }
+    return start
 };
 
-console.log(searchInsert([2,3,5,6], 4))
+// console.log(searchInsert([2,3,5,6], 4))
+// console.log(searchInsert([1,3,5,6], 2))
+// console.log(searchInsert([0,55,58,90,100,126], 78))
+console.log(searchInsert([1,3,5,6], 5))
+console.log(searchInsert([1,3,5,6], 7))
